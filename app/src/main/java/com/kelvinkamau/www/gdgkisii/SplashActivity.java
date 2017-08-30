89faa6b4eb6abc5d3397e6ecd4a54b506e0bad19
Kelvin Kamau <kelvinkamau6808@gmail.com>
2017-08-30 10:16:55 +0300

    Committed loading animation on splash screen.

--- app/src/main/java/com/kelvinkamau/www/gdgkisii/SplashActivity.java
+++ app/src/main/java/com/kelvinkamau/www/gdgkisii/SplashActivity.java
@@ -4,14 +4,23 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class SplashActivity extends AppCompatActivity {
     public static int SPLASH_TIMER = 8000;
+    private AVLoadingIndicatorView avi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        String indicator=getIntent().getStringExtra("indicator");
+        avi= (AVLoadingIndicatorView) findViewById(R.id.avi);
+        avi.setIndicator(indicator);
+        avi.smoothToShow();
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,4 +31,6 @@ public class SplashActivity extends AppCompatActivity {
             }
         } ,SPLASH_TIMER );
     }
+
+
 }
